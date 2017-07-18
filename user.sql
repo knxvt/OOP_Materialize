7dd202199137b4a329bad57506340f7cddf667ce
Alwi zen <knxvt@users.noreply.github.com>
2017-07-18 15:11:16 +0700

    Update user.sql

--- user.sql
+++ user.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 12, 2017 at 02:43 AM
+-- Generation Time: Jul 18, 2017 at 03:10 AM
 -- Server version: 5.6.21
 -- PHP Version: 5.5.19
 
@@ -31,17 +31,19 @@ CREATE TABLE IF NOT EXISTS `user` (
   `nama` varchar(100) NOT NULL,
   `alamat` varchar(100) NOT NULL,
   `no_kamar` char(11) NOT NULL,
-  `email` varchar(50) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+  `email` varchar(50) NOT NULL,
+  `keterangan` varchar(500) NOT NULL,
+  `id_tamu` char(20) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `nama`, `alamat`, `no_kamar`, `email`) VALUES
-(3, 'Alwi', 'Pekalongan', 'M30', 'alwizen@plancode.us'),
-(4, 'Esti', 'Tegal', 'E12', 'esti_wap@plancode.us'),
-(5, 'Krissila', 'Batang', 'K19', 'Krissila@gmail.com');
+INSERT INTO `user` (`id`, `nama`, `alamat`, `no_kamar`, `email`, `keterangan`, `id_tamu`) VALUES
+(5, 'Alwi zen', 'Pekalongan', 'A12', 'alwizen@outlook.co.id', 'Sendirian', '0123'),
+(6, 'Wahyu adi Pangesti', 'Tegal', 'E13', 'esti_wap@gmail.com', 'Bersama Keluarga', '3655'),
+(7, 'Krissila', 'Batang', 'K56', 'krissila@gmail.com', 'Bersama Keluarga', '5558');
 
 --
 -- Indexes for dumped tables
@@ -61,7 +63,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
