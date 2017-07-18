f2f40fd45d45337abac4a773f1714fc9ab2d8b5f
Alwi zen <knxvt@users.noreply.github.com>
2017-07-18 15:10:03 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -4,13 +4,13 @@ $db = new database();
 
 $aksi = $_GET['aksi'];
  if($aksi == "tambah"){
-     $db->input($_POST['nama'],$_POST['alamat'],$_POST['no_kamar'],$_POST['email']);
+     $db->input($_POST['nama'],$_POST['alamat'],$_POST['no_kamar'],$_POST['email'],$_POST['keterangan'],$_POST['id_tamu']);
      header("location:index.php");
  }elseif($aksi == "hapus"){
      $db->hapus($_GET['id']);
     header("location:index.php");
  }elseif($aksi == "update"){
-     $db->update($_POST['id'],$_POST['nama'],$_POST['alamat'],$_POST['no_kamar'],$_POST['email']);
+     $db->update($_POST['id'],$_POST['nama'],$_POST['alamat'],$_POST['no_kamar'],$_POST['email'],$_POST['keterangan'],$_POST['id_tamu']);
      header("location:index.php");
  }
 ?>
